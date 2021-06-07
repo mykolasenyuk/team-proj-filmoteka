ace88bcbf9b843cd70a9d4325b6a84fe4975ae3e
marynazakvatska <maryna.zakvatska@gmail.com>
2021-06-08 00:48:14 +0200

    template

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './sass/_modal_progr_film.scss';
-import onBtnModalImgClose from './js/modal_film';
+/* import onBtnModalImgClose from './js/modal_film'; */
 import './sass/main.scss';
 import './js/modal_film';
+import movieCardTmpl from './templates/cardMovie.hbs';
