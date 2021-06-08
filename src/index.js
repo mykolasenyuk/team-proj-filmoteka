84ff422d9183458604338a0dc8a3b3959f063d95
irynasmile <73856579+irynasmile@users.noreply.github.com>
2021-06-08 15:29:02 +0200

    modai close

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './sass/_modal_progr_film.scss';
-import onBtnModalImgClose from './js/modal_film';
+import './js/modal_film';
 import './sass/main.scss';
 import './js/services/apiService';
 import './js/modal_film';
