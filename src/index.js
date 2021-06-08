8306a1c76219b987ce6b31cb74a75ba8bed55560
irynasmile <73856579+irynasmile@users.noreply.github.com>
2021-06-08 16:51:05 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+<<<<<<< Updated upstream
 import './sass/_modal_progr_film.scss';
 import onBtnModalImgClose from './js/modal_film';
+=======
+import './js/modal_film';
+>>>>>>> Stashed changes
 import './sass/main.scss';
 import './js/services/apiService';
 import './js/modal_film';
