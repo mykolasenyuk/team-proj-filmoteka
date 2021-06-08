c776343ae06ec3f1f0ede94b20e4ad78bd01b621
AnastasiaYun <tigrawom@gmail.com>
2021-06-08 23:36:13 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './sass/main.scss';
 import './js/services/apiService';
 import './js/modal_film';
+import './sass/modal_about_us.scss';
 // import movieCardTmpl from './templates/cardMovie.hbs';
