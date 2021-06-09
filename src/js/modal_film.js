a9bae4a1f7436102f3c78eaac2a91ba9a347dfb6
unknown <maryna.zakvatska@gmail.com>
2021-06-09 23:03:25 +0200

    open modal

--- src/js/modal_film.js
+++ src/js/modal_film.js
@@ -2,8 +2,7 @@
 const refs = {
   backdropModalImg: document.querySelector('.backdrop'),
   btnModalImgClose: document.querySelector('.button__close'),
-  buttonWatched: document.querySelector('.add_watched'),
-  buttonUser: document.querySelector('.add_queue'),
+  moviesCard: document.querySelector('.movies-card'),
 };
 console.log(refs.backdropModalImg);
 console.log(refs.backdropModalImg.classList.value); //backdrop visually-hidden
@@ -28,51 +27,21 @@ function closeModalEscape(e) {
   return;
 }
 
-// btnModalImgClose.addEventListener('click', closeModalClick);
-// function closeModalClick() {
-//   backdropModalImg.classList.add('is-close');
-//
-
-const listWatched = document.createElement('ul');
-listWatched.classList.add('modal_list_watched');
-
-const listWillWatch = document.createElement('ul');
-listWillWatch.classList.add('modal_list_willwatch');
-
-/* FT - 18 По нажатию на кнопку "Add to watched"
-фильм добавляется в просмотренные
-фильмы текущего пользователя(local - storage) */
-
-/* FT - 19 По нажатию на кнопку "Add to queue"
-фильм добавляется
-в очередь текущего пользователя(local - storage) */
-
-/* console.log(localStorage); */
 
 
-// refs.buttonWatched(click, onAddWatched);
-// function onAddWatched() {
-//   console.log(e.currentTarget);
-//   const movie = e.currentTarget;
-//   localStorage.setItem(movie);
-//   listWatched.insertAdjacentHTML('beforeend', localStorage.setItem(movie));
-// }
-// refs.buttonUser(click, onHaveToWatch);
-
-function closeModalCardMovie(e) {
-  const onOverlayCloseBtn = e.target !== lightboxCloseBtn;
-  /* console.log(e.currentTarget); */
-  if (onOverlayCloseBtn) {
-    removeLightboxIsOpen();
-    removeAtributes();
+refs.moviesCard.addEventListener("click", addOpenLightboxClass);
+function addOpenLightboxClass(e) {
+  e.preventDefault();
+  if (e.target.localName !== "img") {
+    return;
   }
+  addOpenLightboxClass();
+ /*  addAttributes(e); */
 }
-function removeLightboxIsOpen() {
-  lightbox.classList.remove('is-open');
+function addOpenLightboxClass() {
+  refs.backdropModalImg.classList.add("is-open");
 }
-
-function removeAtributes() {
-  lightBoxImage.src = '';
-  lightBoxImage.alt = '';
-}
-
+/* function addAttributes(evt) {
+  lightBoxImage.src = evt.target.dataset.source;
+  lightBoxImage.alt = evt.target.alt;
+} */
