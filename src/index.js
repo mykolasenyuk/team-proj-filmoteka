bc48defe554c6807e6da9d47acfe8387c3978509
irynasmile <73856579+irynasmile@users.noreply.github.com>
2021-06-12 22:50:39 +0200

    styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import './sass/main.scss';
 import './js/services/apiService';
-// import './js/modal_film';
+import './js/modal_film';
 import './js/hero_movies';
 import './js/onSearch';
-
-// import movieCardTmpl from './templates/cardMovie.hbs';
 
 import './js/about_us';
 import './js/my-library';
--- src/js/modal_film.js
+++ src/js/modal_film.js
@@ -1,13 +1,11 @@
-
 import movieCardTmpl from '../templates/cardMovie.hbs';
 import moviesList from '../templates/hero_movies.hbs';
 import ApiService from './services/apiService';
 // console.log(moviesList)
-const apiService = new ApiService;
+const apiService = new ApiService();
 const refs = {
   backdropModalImg: document.querySelector('.backdrop'),
   btnModalImgClose: document.querySelector('.button__close'),
-  
 };
 // console.log(refs.backdropModalImg);
 // console.log(refs.backdropModalImg.classList.value); //backdrop visually-hidden
@@ -15,25 +13,24 @@ refs.backdropModalImg.addEventListener('click', onBackdropModalClose);
 addEventListener('click', onBtnModalClose);
 
 // Откритие модалки
-document.querySelector('.movies-card');
+// document.querySelector('.movies-card');
 addEventListener('click', onOpenModalFilmCard);
 function addOpenLightboxClass() {
-  refs.backdropModalImg.classList.add("is-open");
+  refs.backdropModalImg.classList.add('is-open');
   refs.backdropModalImg.classList.remove('is-hidden');
-} 
+}
 
 function onBackdropModalClose(e) {
   if (e.target.classList.value === 'backdrop') {
-    refs.backdropModalImg.classList.remove("is-open");
+    refs.backdropModalImg.classList.remove('is-open');
     refs.backdropModalImg.classList.add('is-hidden');
     console.log(e.target.classList.value);
   }
   return;
 }
 
-
 function onBtnModalClose(e) {
-  refs.backdropModalImg.classList.remove("is-open");
+  refs.backdropModalImg.classList.remove('is-open');
   refs.backdropModalImg.classList.add('is-hidden');
   // console.log(e.target);
 }
@@ -42,40 +39,36 @@ window.addEventListener('keydown', closeModalEscape);
 
 function closeModalEscape(e) {
   if (e.code === 'Escape') {
-    refs.backdropModalImg.classList.remove("is-open");
+    refs.backdropModalImg.classList.remove('is-open');
     refs.backdropModalImg.classList.add('is-hidden');
   }
   return;
 }
 
-
-
 function onOpenModalFilmCard(e) {
-  
   e.preventDefault();
-  
+
   if (e.target.nodeName !== 'IMG') {
     return;
   }
-  const movieId = Number(e.target.dataset.action)
-  console.log(movieId)
+  const movieId = Number(e.target.dataset.action);
+  console.log(movieId);
 
-  addOpenLightboxClass()
-  apiService.getModalMovie(movieId).then(data => renderModal(data))
-};
+  addOpenLightboxClass();
+  apiService.getModalMovie(movieId).then(data => renderModal(data));
+}
 
 const renderModal = data => {
   const modalMarkapMovieCard = movieCardTmpl(data);
-  console.log(modalMarkapMovieCard)
-  refs.backdropModalImg.insertAdjacentHTML('beforeend',modalMarkapMovieCard);
-}
+  console.log(modalMarkapMovieCard);
+  refs.backdropModalImg.insertAdjacentHTML('beforeend', modalMarkapMovieCard);
+};
 // movie/${movie_id}?api_key=${apiKey}
- 
 
 // /* function movieCardInfo(data) {
 //   listEl.insertAdjacentHTML("beforeend", movieCardTmpl(data));
- 
+
 // }
-// function clearCardList() {
-//   listEl.innerHTML = "";
-// } */
+function clearCardList() {
+  listEl.innerHTML = '';
+}
