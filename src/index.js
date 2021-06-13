64d07a86a363c0ad2fc6256429c4e04971253b99
mykolasenyuk <73036879+mykolasenyuk@users.noreply.github.com>
2021-06-13 18:18:50 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './sass/main.scss';
 import './js/services/apiService';
 import './js/pagination';
-// import './js/modal_film';
+import './js/modal_film';
 import './js/hero_movies';
 import './js/onSearch';
 // import './js/homePage';
