3c4104d85df168b61fc1a083fd092c8465fb945b
Vadim Brikulya <im.master99@gmail.com>
2021-06-15 00:29:40 +0300

    up

--- src/js/my-library.js
+++ src/js/my-library.js
@@ -6,14 +6,18 @@ import apiService from './services/apiService';
 
 refs.libraryBtn.addEventListener('click', (event) => {
     
-    refs.searchWrap.classList.add('visually-hidden');
-    refs.libraryBtnsContainer.classList.remove('visually-hidden');
-    refs.libraryBtn.classList.add('current');
-    refs.homeBtn.classList.remove('current');
+    refs.searchWrap.classList.add('visually-hidden');//cкрывается поиск инпут    
+    refs.libraryBtn.classList.add('current');//подчеркивание library
+
+    refs.libraryBtnsContainer.classList.remove('visually-hidden');// показывают кнопки
+    refs.homeBtn.classList.remove('current');//убирается подчеркивание c кнопки library
     
-    refs.headerBg.classList.add('visually-hidden');
+    refs.headerBg.classList.toggle('library__background');
+
     refs.libraryBg.classList.remove('visually-hidden');
     refs.homeBtn.classList.remove('visually-hidden')
+
+    
 });
 
 refs.watchedBtn.addEventListener('click', (event) => {
@@ -30,6 +34,6 @@ refs.homeBtn.addEventListener('click', (event) => {
     refs.libraryBtn.classList.remove('current');
     refs.searchWrap.classList.remove('visually-hidden');
     refs.libraryBtnsContainer.classList.add('visually-hidden');
-    
+    refs.headerBg.classList.toggle('library__background');
 }
 )
