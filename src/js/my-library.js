0ba44a7fa85e13b33d268a11a95634c9490d4588
mykolasenyuk <73036879+mykolasenyuk@users.noreply.github.com>
2021-06-17 01:30:42 +0100

    addedfixes

--- src/js/my-library.js
+++ src/js/my-library.js
@@ -1,74 +1,81 @@
 import refs from './refs/refs';
 import apiService from './services/apiService';
-import moviesList from "../templates/hero_movies.hbs";
-import {stopSpin} from "./spinner/spinner";
-import localStorage from './services/localStorage'
+import moviesList from '../templates/hero_movies.hbs';
+import { stopSpin } from './spinner/spinner';
+import localStorage from './services/localStorage';
 
-let storage = new localStorage()
- 
+let storage = new localStorage();
+
 const moviesContainer = document.querySelector('.js-movies-container');
- 
+
 function clearMarkup() {
   moviesContainer.innerHTML = '';
 }
- 
+
 function renderFilmsCard(data) {
   moviesContainer.insertAdjacentHTML('beforeend', moviesList(data));
   const moviesCardVoteEl = document.querySelectorAll('.movies-card-vote');
   moviesCardVoteEl.forEach(classList => {
-    classList.classList.add("is-hidden");
+    classList.classList.add('is-hidden');
   });
-
+  // console.log(moviesList(data));
   stopSpin();
-
 }
 
-
-
-
 refs.libraryBtn.addEventListener('click', () => {
-    
-  clearMarkup()
-  watchedFilmsRender()
-  refs.searchWrap.classList.add('visually-hidden');//cкрывает поиск инпут
+  clearMarkup();
+  watchedFilmsRender();
+  refs.searchWrap.classList.add('visually-hidden'); //cкрывает поиск инпут
 
-    refs.libraryBtn.classList.add('current');//подчеркивание library
+  refs.libraryBtn.classList.add('current'); //подчеркивание library
 
-    refs.libraryBtnsContainer.classList.remove('visually-hidden');// показывает кнопки
-    
-    refs.homeBtn.classList.remove('current');//убирает подчеркивание c кнопки home
-    
-    refs.headerBg.classList.add('library__background');//добавляет фон library
+  refs.libraryBtnsContainer.classList.remove('visually-hidden'); // показывает кнопки
 
-    refs.headerBg.classList.remove('header__background');   //скрывает фон header
+  refs.homeBtn.classList.remove('current'); //убирает подчеркивание c кнопки home
 
-    refs.paginationContainer.classList.add('disactive-pagination'); //скрывает пагинацию кнопок
+  refs.headerBg.classList.add('library__background'); //добавляет фон library
 
+  refs.headerBg.classList.remove('header__background'); //скрывает фон header
+
+  refs.paginationContainer.classList.add('disactive-pagination'); //скрывает пагинацию кнопок
 });
 
 refs.watchedBtn.addEventListener('click', watchedFilmsRender);
 refs.queueBtn.addEventListener('click', queueFilmsRender);
 
 function watchedFilmsRender() {
-  clearMarkup()
-  renderFilmsCard(storage.getWatched())// Делает карточки Watched
-  refs.watchedBtn.classList.add('active')
-  refs.queueBtn.classList.remove('active')
-
-};
-
+  clearMarkup();
+  renderFilmsCard(
+    storage.getWatched().map(film => ({
+      ...film,
+      release_date: film.release_date.slice(0, 4),
+    })),
+  ); // Делает карточки Watched
+  refs.watchedBtn.classList.add('active');
+  refs.queueBtn.classList.remove('active');
+}
+// console.log(
+//   storage.getWatched().map(film => ({
+//     ...film,
+//     release_date: film.release_date.slice(0, 4),
+//   })),
+// );
 function queueFilmsRender() {
-  clearMarkup()
-  renderFilmsCard(storage.getQueue())// Делает карточки Queue
-  refs.queueBtn.classList.add('active')
-  refs.watchedBtn.classList.remove('active')
-
+  clearMarkup();
+  renderFilmsCard(
+    storage.getQueue().map(film => ({
+      ...film,
+      release_date: film.release_date.slice(0, 4),
+    })),
+  ); // Делает карточки Queue
+  refs.queueBtn.classList.add('active');
+  refs.watchedBtn.classList.remove('active');
 }
 
-
-
-refs.homeBtn.addEventListener('click', (event) => {
-    
+refs.homeBtn.addEventListener('click', onLogoAndHome);
+refs.logo.addEventListener('click', onLogoAndHome);
+function onLogoAndHome() {
+  {
     refs.homeBtn.classList.add('current');
 
     refs.libraryBtn.classList.remove('current');
@@ -80,10 +87,9 @@ refs.homeBtn.addEventListener('click', (event) => {
     refs.headerBg.classList.remove('library__background');
 
     refs.headerBg.classList.add('header__background');
-    
+
     refs.heroBlock.classList.remove('visually-hidden');
 
     refs.paginationContainer.classList.remove('disactive-pagination');
+  }
 }
-)
-
--- src/js/refs/refs.js
+++ src/js/refs/refs.js
@@ -1,16 +1,14 @@
-
 const refs = {
-  
   searchInputRef: document.querySelector('input[name="search"]'),
-  
 
+  logo: document.querySelector('.logo'),
   homeBtn: document.querySelector('#home-button'),
   libraryBtn: document.querySelector('#library-button'),
   headerBg: document.querySelector('.header__background'),
   searchWrap: document.querySelector('.search__input-wrapper'),
   libraryBtnsContainer: document.querySelector('.button-container'),
-  libraryBg:document.querySelector('.library__background'),
-  
+  libraryBg: document.querySelector('.library__background'),
+
   heroBlock: document.querySelector('.hero'),
   paginationContainer: document.querySelector('.pagination__container'),
   filterInput: document.querySelectorAll('.filter-input'),
@@ -19,7 +17,6 @@ const refs = {
   queueBtn: document.querySelector('.queue-button'),
   filter: document.querySelector('.filter-section'),
   searchInput: document.querySelector('.search__input'),
-  
 };
 
 export default refs;
