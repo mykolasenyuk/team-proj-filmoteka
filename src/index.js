e1338780e78b77bea628860f53f246cacfc4a9df
marinadumberg <marina.dumberg@inbox.lv>
2021-06-18 13:18:15 +0300

    fixes

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import './sass/main.scss';
 import './js/services/apiService';
 import './js/pagination';
 
-import './js/hero_movies';
-import './js/modal_film';
+import './js/heroMovies';
+import './js/modalFilm';
 import './js/onSearch';
 
-import './js/about_us';
-import './js/my-library';
+import './js/aboutUs';
+import './js/myLibrary';
 import './js/backToTop'
